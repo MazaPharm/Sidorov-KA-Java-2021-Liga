a25886ac8e3f3a78fc9c4c13a83ca0113002f37f
MazaPharm <kirill28128@yandex.ru>
2021-09-11 13:09:18 +0300

    Sidorov-KA-Java-2021-Liga

---
+++ src/main/java/ru/philosophyit/internship/javabase/files/Main.java
@@ -0,0 +1,54 @@
+package ru.philosophyit.internship.javabase.files;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    private static StringBuilder fileWays;
+
+    public static void main(String ...args) throws IOException {
+        System.out.println(Files.readString(Path.of("src/main/resources/hello.txt")));
+
+        System.out.println("1-Й СПОСОБ");
+        Files.walk(Paths.get("src"))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+
+        System.out.println("\n");
+        System.out.println("2-Й СПОСОБ");
+        File file = new File("C:\\Users\\Tsar\\Desktop\\Java Liga\\javabase\\src");
+        fileWays = new StringBuilder();
+        allWays(file);
+
+        // Отобразите рекурсивно дерево директорий src/main/java/ru/philosophyit/internship/javabase
+        // например 
+        // src/main/java/ru/philosophyit/internship/javabase/files/Main.java
+        // src/main/java/ru/philosophyit/internship/javabase/locks/DeadLock.java
+        // src/main/java/ru/philosophyit/internship/javabase/locks/LiveLock.java
+        // src/main/java/ru/philosophyit/internship/javabase/threads/Completable.java
+        // и т.д.
+        /// Более удачные оформления вывода в консоль приветствуются
+    }
+
+    private static void allWays(File file){
+        for (File f:file.listFiles()) {
+            if(f.isDirectory()){
+                allWays(f);
+            } else {
+                fileWays.append(f);
+                System.out.println(fileWays.toString());
+                fileWays.setLength(0);
+
+            }
+
+        }
+    }
+}
---
+++ src/main/java/ru/philosophyit/internship/javabase/time/Main.java
@@ -0,0 +1,96 @@
+package ru.philosophyit.internship.javabase.time;
+
+import java.text.DateFormatSymbols;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
+public class Main {
+
+    private static StringBuilder days;
+
+    public static void main(String... args) {
+
+        System.out.println(DateTimeFormatter.ISO_INSTANT.format(Calendar.getInstance().toInstant()));
+
+        nameOfDays();
+        daysOfMonth();
+
+
+        // Отобразите календарь текущего месяца в консоли
+        // например:
+        // пн вт ср чт пт сб вс
+        // 30 31 1  2  3  4  5
+        // и т.д.
+    }
+
+    private static void nameOfDays() {
+        String[] namesOfDays = DateFormatSymbols.getInstance().getShortWeekdays();
+        StringBuilder dayOfWeks = new StringBuilder();
+        for (int i = 2; i < namesOfDays.length; i++) {
+            dayOfWeks.append(namesOfDays[i]);
+            dayOfWeks.append(" ");
+        }
+        dayOfWeks.append(namesOfDays[1]);
+        System.out.println(dayOfWeks.toString());
+    }
+
+    private static void daysOfMonth() {
+        Calendar calendar = Calendar.getInstance();
+        int month = calendar.get(Calendar.MONTH);
+        int countDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        calendar.set(2021, month, 1);
+        int dayInBegin = calendar.get(Calendar.DAY_OF_WEEK);
+        calendar.set(2021, month - 1, 1);
+        int daysInPreviousMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        tableOfDays(countDays, dayInBegin, daysInPreviousMonth);
+    }
+
+    private static void tableOfDays(int countDays, int dayInBegin, int daysInPreviousMonth) {
+        days = new StringBuilder();
+        int d = 0;
+        int prvsMonth = 0;
+        if (dayInBegin == 1) {
+            prvsMonth = 6;
+        } else
+            prvsMonth = (7 - (7 - dayInBegin + 1)) - 1;
+        int k = 1;
+        int limit = 35 - prvsMonth;
+        while (k <= limit) {
+            while (prvsMonth > 0) {
+                days.append((31 - prvsMonth) + 1);
+                d++;
+                days.append(" ");
+                prvsMonth--;
+                if (d == 7) {
+                    d = 0;
+                    days.append("\n");
+                }
+            }
+            days.append(k);
+            d++;
+            days.append(" ");
+            if (k < 10) days.append(" ");
+            if (d == 7) {
+                d = 0;
+                days.append("\n");
+            }
+            if (k >= countDays) {
+                k = 1;
+                while (k <= (limit - countDays)) {
+                    days.append(k);
+                    d++;
+                    days.append(" ");
+                    days.append(" ");
+                    if (d == 7) {
+                        d = 0;
+                        days.append("\n");
+                    }
+                    k++;
+                }
+                break;
+            }
+            k++;
+        }
+        System.out.println(days.toString());
+    }
+}
