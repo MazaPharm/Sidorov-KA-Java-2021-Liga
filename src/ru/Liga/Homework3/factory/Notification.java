9f77f3338143a2d5b86e803c3131a80356e831a0
MazaPharm <kirill28128@yandex.ru>
2021-09-21 00:20:55 +0300

    Sidorov-KA-Java-2021-Liga

--- src/ru/Liga/Homework3/factory/Notification.java
+++
@@ -1,5 +0,0 @@
-package ru.Liga.Homework3.factory;
-
-public interface Notification {
-   public String getText();
-}
--- src/ru/Liga/Homework3/factory/NotificationFactory.java
+++
@@ -1,7 +0,0 @@
-package ru.Liga.Homework3.factory;
-
-import ru.Liga.Homework3.Decorator.User;
-
-public interface NotificationFactory {
-    public Notification getNotification(String language);
-}
