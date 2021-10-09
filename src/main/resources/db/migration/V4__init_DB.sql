3f2017cfa3853186266da7ae50e4e9c8dc496c22
MazaPharm <kirill28128@yandex.ru>
2021-10-09 14:55:02 +0300

    a

--- src/main/resources/db/migration/V4__init_DB.sql
+++
@@ -1,7 +0,0 @@
-create table usr(
-    id integer not null primary key,
-    name varchar(40),
-    surname varchar(40),
-    school_id integer,
-    foreign key (school_id) references school (id) on delete set null
-);
